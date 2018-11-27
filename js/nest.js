6199a811d426b1fdc7775291b9989a4ba4c6736e
hongrong <ghrace@live.com>
2018-11-27 11:53:55 +0800

    color

--- js/nest.js
+++ js/nest.js
@@ -15,7 +15,7 @@
             l: w,
             z: o(v, "zIndex", -1),
             o: o(v, "opacity", 0.7),
-            c: o(v, "color", "255,50,200"),
+            c: o(v, "color", "0,0,255"),
             n: o(v, "count", 120)
         }
     }
